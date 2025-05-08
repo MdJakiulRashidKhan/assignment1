6c379408ad495a9c925e225090eebb628404ade3
MdJakiulRashidKhan <jakiul15-8554@diu.edu.bd>
2025-05-08 13:40:31 +0600

    example remove

--- module/src/assignment1.ts
+++ module/src/assignment1.ts
@@ -100,5 +100,4 @@ async function squareAsync(n: number): Promise<number> {
   });
 }
 
-squareAsync(4).then(console.log);        // Output after 1s: 16
-squareAsync(-3).catch(console.error);    // Output: Error: Negative number not allowed
+
